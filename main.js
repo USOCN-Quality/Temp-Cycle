9b27201d58fcdad9a278e7579089dd87cfdc2c0f
MALVAR Eric <eric.malvar@hexagon.com>
2022-02-02 11:42:05 -0800

    added functionality to calculate datetimes based on an array

--- main.js
+++ main.js
@@ -1,59 +1,35 @@
 $(document).ready(()=>{
-    $('#example1').calendar();
-    $('#example2').calendar({
-      type: 'date'
-    });
-    $('#example3').calendar({
-      type: 'time'
-    });
-    $('#rangestart').calendar({
-      type: 'date',
-      endCalendar: $('#rangeend')
-    });
-    $('#rangeend').calendar({
-      type: 'date',
-      startCalendar: $('#rangestart')
-    });
-    $('#example4').calendar({
-      startMode: 'year'
-    });
-    $('#example5').calendar();
-    $('#example6').calendar({
-      ampm: false,
-      type: 'time'
-    });
-    $('#example7').calendar({
-      type: 'month'
-    });
-    $('#example8').calendar({
-      type: 'year'
-    });
-    $('#example9').calendar();
-    $('#example10').calendar({
-      on: 'hover'
-    });
-    var today = new Date();
-    $('#example11').calendar({
-      minDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() - 5),
-      maxDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() + 5)
-    });
-    $('#example12').calendar({
-      monthFirst: false
-    });
-    $('#example13').calendar({
-      monthFirst: false,
-      formatter: {
-        date: function (date, settings) {
-          if (!date) return '';
-          var day = date.getDate();
-          var month = date.getMonth() + 1;
-          var year = date.getFullYear();
-          return day + '/' + month + '/' + year;
+    var insertTimes = [8,12,16,20,24,32,44]
+    $("#checkTimes").on("click",function(){
+        var dateTime = $("#dateInput").val()
+        if(dateTime ===""){
+            alert("Please put in a date/time value")
+        }else if(dateTime.match(/\w* \d, \d\d\d\d/)[0]){
+            //alert(AddTimes(dateTime,16))
+            $("#addTimesHere").empty()
+            insertTimes.forEach(item=>{
+                var insertTimesString ="<p>"+AddTimes(dateTime,item)+"</p>"
+               
+                $("#addTimesHere").append(insertTimesString)
+               
+            })
         }
+    })
+
+    var AddTimes = function(date1, h){
+        var dt = new Date(date1)
+        dt.setHours(dt.getHours()+ h)
+        // var dateString = dt.toLocaleString('en-us', {  weekday: 'long' })+" "+ dt.getDay() +"/"+dt.getMonth()+"/"+dt.getFullYear()+ " " + formatAMPM(dt)
+        return dt.toString().split(" GMT")[0];
+    }
+    var formatAMPM = function (date) {
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+        return strTime;
       }
-    });
-    $('#example14').calendar({
-      inline: true
-    });
-    $('#example15').calendar();
 })
